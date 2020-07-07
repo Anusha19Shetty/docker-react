055bdc79301b5c23cfcb5c20ec2e74739e8dfe8a
root <anusha19shetty@gmail.com>
2020-07-07 14:36:47 +0530

    commit by anusha

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Anusha Editing  <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Ha Ha Ha Anusha,  Learn React !!
         </a>
       </header>
     </div>
