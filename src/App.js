d08dcaaad200dd2482cd64a193c363ed702196a0
root <anusha19shetty@gmail.com>
2020-07-08 12:55:40 +0530

    updated js files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Anusha Editing  <code>src/App.js</code> and save to reload.
+         Checking Deployment Anusha Editing  <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
